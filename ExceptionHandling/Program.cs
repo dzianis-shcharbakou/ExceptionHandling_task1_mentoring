d8d9875d7c6d1f53a52ed446f97cf7f90e949de0
Den <Dzianis_Shcharbakou@epam.com>
2020-02-27 09:46:35 +0300

    Complete

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ExceptionHandling
 {
@@ -6,7 +7,24 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Typing data:");
+            StringBuilder resultString = new StringBuilder();
+
+            for (int i = 0; i<6;i++)
+            {
+                var typingString = Console.ReadLine();
+                try
+                {
+                    resultString.Append(typingString[0]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Exception: Type any symbols!");
+                    --i;
+                }
+            }
+
+            Console.WriteLine(resultString);
         }
     }
 }
